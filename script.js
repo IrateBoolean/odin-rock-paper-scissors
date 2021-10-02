31d9c0581eca110bf357ee984028767c3c8b5359
KJ <irateboolean@gmx.com>
2021-10-02 13:49:56 -0400

    added computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Let's Play: Rock Paper Scissors!")
+
+CHOICES = ["Rock", "Paper", "Scissors"]
+
+function getRandom(arr){
+  idx = Math.floor(Math.random() * arr.length)
+  return arr[idx]
+}
+
+
+function computerPlay(){
+  return getRandom(CHOICES)
+}
+
