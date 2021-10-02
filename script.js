8ae4c5dbe23e88d2feb345d0e640979af75d63ee
KJ <irateboolean@gmx.com>
2021-10-02 13:59:25 -0400

    added playRound

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function computerPlay(){
   return getRandom(CHOICES)
 }
 
+function playRound(playerSelection, computerSelection){
+  if (playerSelection == computerSelection) {
+    return "It's a tie! What are the odds? One in three of course."
+  } else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+             (playerSelection == "Paper" && computerSelection == "Scissors") ||
+             (playerSelection == "Scissors" && computerSelection == "Rock")) {
+    return `You lose! ${computerSelection} beats ${playerSelection}`
+  } else {
+    return `Congratulations! ${playerSelection} beats ${computerSelection}`
+  }
+
+}
+
